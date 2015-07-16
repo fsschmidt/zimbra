71b17a7f8cce29118e7b27b52a170d4a08fcaef1
Fabio Soares Schmidt <fabio@improve.inf.br>
2015-07-15 23:55:50 -0300

    Create exporta_mysql.sh

---
+++ scripts/exporta_mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#EXPORTAR TABELA ZIMBRA E TABELAS MBOXGROUP
+#FABIO S. SCHMIDT - fabio.schmidt@4linux.com.br - 02/04/2015
+#TODO:
+
+DEST="/tmp"
+
+EXPORTAR=`mysql -u root --password=$mysql_root_password --socket=$mysql_socket -N information_schema -e "SELECT DISTINCT(TABLE_SCHEMA) FROM tabl
+es WHERE TABLE_SCHEMA LIKE 'mboxgroup%'"`
+
+#EXPORTAR TABELA ZIMBRA
+echo "EXPORTANDO TABELA ZIMBRA"
+/opt/zimbra/mysql/bin/mysqldump --user=root --password=$mysql_root_password --socket=$mysql_socket   --compatible=ansi --single-transaction --fl
+ush-logs zimbra > $DEST/base_zimbra.sql
+
+#EXPORTAR TABELAS MAILBOX
+
+printf "TABELAS MAILBOX QUE SERAO EXPORTADAS: \n$EXPORTAR \n"
+echo "EXPORTANDO TABELAS MAILBOX"
+/opt/zimbra/mysql/bin/mysqldump --user=root --password=$mysql_root_password --socket=$mysql_socket --compatible=ansi --single-transaction --flus
+h-logs --databases $(mysql -u root --password=$mysql_root_password --socket=$mysql_socket -N information_schema -e "SELECT DISTINCT(TABLE_SCHEMA
+) FROM tables WHERE TABLE_SCHEMA LIKE 'mboxgroup%'") > $DEST/bases_mboxgroup.sql
