4af13a0fc34c015ccc5f2b0d86355255f2ccf23a
Fabio Soares Schmidt <fabio@improve.inf.br>
2017-04-28 21:45:37 -0300

    Update soma_quota.sh

--- soma_quota.sh
+++ soma_quota.sh
@@ -1,1 +1,4 @@
 /opt/zimbra/bin/zmprov gqu `zmhostname` | grep "@zimbra0.lab" | cut -d " " -f 3 | sed -e "s/ /,/g" | paste -sd+ - | bc
+
+zmprov gqu `zmhostname` | grep "@zimbra0.lab" | cut -d " " -f 3 | sed -e "s/ /,/g" | paste -sd+ - | bc | awk '{ byte = $1 /1024 ; print byte " k";byte = $1 /1024/1024 ; print byte " MB"; byte =$1 /1024/1024**2 ; print byte " GB" }'
+
