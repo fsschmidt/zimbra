d74f18067b8449f75715ba0ca27cbe556f55f6ba
Fabio Soares Schmidt <fabio@improve.inf.br>
2019-08-01 22:15:35 -0300

    Update exportar_mysql.sh

--- exportar_mysql.sh
+++ exportar_mysql.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #EXPORTAR TABELA ZIMBRA E TABELAS MBOXGROUP
 #FABIO S. SCHMIDT - fabio@respirandolinux.com.br - 02/04/2015
+# VALIDADO ZIMBRA 8.8.15 em 01/08/2019
 #TODO:
 
 DEST="/tmp"
@@ -13,10 +14,10 @@ EXPORTAR=`mysql -u root --password=$mysql_root_password --socket=$mysql_socket -
 
 #EXPORTAR TABELA ZIMBRA
 echo "EXPORTANDO TABELA ZIMBRA"
-/opt/zimbra/mysql/bin/mysqldump --user=root --password=$mysql_root_password --socket=$mysql_socket   --compatible=ansi --single-transaction --flush-logs zimbra > $DEST/base_zimbra.sql
+mysqldump --user=root --password=$mysql_root_password --socket=$mysql_socket   --compatible=ansi --single-transaction --flush-logs zimbra > $DEST/base_zimbra.sql
 
 #EXPORTAR TABELAS MAILBOX
 
 printf "TABELAS MAILBOX QUE SERAO EXPORTADAS: \n$EXPORTAR \n"
 echo "EXPORTANDO TABELAS MAILBOX"
-/opt/zimbra/mysql/bin/mysqldump --user=root --password=$mysql_root_password --socket=$mysql_socket --compatible=ansi --single-transaction --flush-logs --databases $(mysql -u root --password=$mysql_root_password --socket=$mysql_socket -N information_schema -e "SELECT DISTINCT(TABLE_SCHEMA) FROM tables WHERE TABLE_SCHEMA LIKE 'mboxgroup%'") > $DEST/bases_mboxgroup.sql
+mysqldump --user=root --password=$mysql_root_password --socket=$mysql_socket --compatible=ansi --single-transaction --flush-logs --databases $(mysql -u root --password=$mysql_root_password --socket=$mysql_socket -N information_schema -e "SELECT DISTINCT(TABLE_SCHEMA) FROM tables WHERE TABLE_SCHEMA LIKE 'mboxgroup%'") > $DEST/bases_mboxgroup.sql
